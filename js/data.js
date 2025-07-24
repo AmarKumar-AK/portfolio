d36d6ef3698484b444a4e8fe9390ae18283e8de0
Amar Kumar <nokhlalkumar@Amars-MacBook-Pro.local>
2025-07-24 23:17:13 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,80 @@
+const portfolioData = {
+  personalInfo: {
+    name: "Amar Kumar",
+    title: "Full stack Developer | Software Engineer at Publicis Sapient",
+    location: "Gaya, Bihar, India",
+    phone: "9110129748",
+    email: "9801amarkumar@gmail.com",
+    linkedin: "www.linkedin.com/in/amar-kumar-0b25b0171",
+    photo: "https://randomuser.me/api/portraits/men/32.jpg" // Temporary photo from web
+  },
+  skills: [
+    "React.js",
+    "Redux.js",
+    "Spring Boot",
+    "Java",
+    "JavaScript",
+    "HTML/CSS"
+  ],
+  certifications: [
+    "Cloud Digital Leader",
+    "ADSE Java Full Stack",
+    "AWS Certified Cloud Practitioner",
+    "AWS Cloud Practitioner Essentials",
+    "SQL & PostgreSQL for Beginners: Become an SQL Expert"
+  ],
+  experience: [
+    {
+      company: "Publicis Sapient",
+      roles: [
+        {
+          title: "Associate Software development engineer 2",
+          period: "May 2024 - Present (1 year 3 months)",
+          location: "India",
+          description: ""
+        },
+        {
+          title: "Associate Software Development Engineer 1",
+          period: "April 2023 - April 2024 (1 year 1 month)",
+          location: "India",
+          description: ""
+        },
+        {
+          title: "Trainee engineer",
+          period: "June 2022 - March 2023 (10 months)",
+          location: "India",
+          description: ""
+        }
+      ]
+    },
+    {
+      company: "Glabbr Technologies Private Limited",
+      roles: [
+        {
+          title: "Software Engineer",
+          period: "June 2021 - December 2021 (7 months)",
+          location: "Maharashtra, India",
+          description: "I worked on a microservice called Document Management System where I wrote different APIs for opening, copying, moving, deleting documents and many more."
+        }
+      ]
+    },
+    {
+      company: "Indian Institute of Information Technology Design & Manufacturing Kancheepuram",
+      roles: [
+        {
+          title: "Student",
+          period: "August 2017 - July 2022 (5 years)",
+          location: "Chennai, Tamil Nadu, India",
+          description: ""
+        }
+      ]
+    }
+  ],
+  education: [
+    {
+      institution: "Indian Institute of Information Technology Design & Manufacturing Kancheepuram",
+      degree: "Bachelor's degree, Computer Engineering",
+      period: "2017 - 2022"
+    }
+  ]
+};
